803296afcef584c9711d3ca2c35d2fcc5235dc67
Ben Mohammad <benmohammad593@gmail.com>
2020-04-21 00:11:59 +0100

    Initial commit

---
+++ app/src/main/java/com/reactiveapplication/persistence/UserDao.java
@@ -0,0 +1,23 @@
+package com.reactiveapplication.persistence;
+
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM Users LIMIT 1 ")
+    Flowable<User> getUser();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable insertUser(User user);
+
+    @Query("DELETE FROM Users")
+    void deleteAllUsers();
+}
